c66a0775ba2889280e00d7d0a659604cbcd6a945
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-07-07 00:44:54 -0300

    feat(markdown): add component

---
+++ src/components/Markdown.tsx
@@ -0,0 +1,20 @@
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import ReactMarkdown from 'markdown-to-jsx';
+import React, { Props } from 'react';
+
+const options = {
+  overrides: {
+    h1: { component: Typography, props: { variant: 'h1' } },
+    h2: { component: Typography, props: { variant: 'h2' } },
+    h3: { component: Typography, props: { variant: 'h3' } },
+    h4: { component: Typography, props: { variant: 'h4' } },
+    span: { component: Typography, props: { variant: 'caption', paragraph: true } },
+    p: { component: Typography, props: { variant: 'body1', paragraph: true } },
+    a: { component: Link },
+  },
+};
+
+export default function Markdown(props: Props<unknown>) {
+  return <ReactMarkdown options={options} {...props} />;
+}
