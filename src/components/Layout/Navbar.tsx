3c468379af3a96fb1846a0fe5190b1a3ac5a89b5
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-07-09 10:17:18 -0300

    style: fix lint warnings

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,6 +1,5 @@
 import { makeStyles } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
