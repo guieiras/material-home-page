4e769b9a8e3289926c6d3a020c8956f080f5c3d9
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-07-15 13:33:36 -0300

    fix(serializer): allow group to be null without raise errors

--- src/serializers/contentful/skills.ts
+++ src/serializers/contentful/skills.ts
@@ -9,16 +9,18 @@ export default function ContentfulSkillSerializer(items: ContentfulEntries['cont
     .sort((a, b) => (a.fields.skill > b.fields.skill ? 1 : a.fields.skill < b.fields.skill ? -1 : 0))
     .forEach(({ fields }) => {
       const group = fields.group as ContentfulEntries['content']['items'][0];
-      skillGroups[group.sys.id as string] = skillGroups[group.sys.id as string] || {
-        name: group.fields.name as string,
-        order: group.fields.order as number,
-        skills: [],
-      };
+      if (group) {
+        skillGroups[group.sys.id as string] = skillGroups[group.sys.id as string] || {
+          name: group.fields.name as string,
+          order: group.fields.order as number,
+          skills: [],
+        };
 
-      skillGroups[group.sys.id as string].skills.push({
-        name: fields.skill as string,
-        tags: fields.tags as Skill['tags'],
-      });
+        skillGroups[group.sys.id as string].skills.push({
+          name: fields.skill as string,
+          tags: fields.tags as Skill['tags'],
+        });
+      }
     });
 
   return Object.values(skillGroups).sort((a, b) => a.order - b.order);
