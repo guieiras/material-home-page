76b8eb5394f60773fed0a695b61541c84d70169e
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-12-21 09:59:40 -0300

    fix(layout): move meta from _document to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
     <React.Fragment>
       <Head>
         <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
