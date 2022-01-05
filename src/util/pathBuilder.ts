df5ce31d8ef91129d03bd41453f87bf9bcbc3397
Guilherme Eiras <guialbertoeiras@hotmail.com>
2022-01-05 16:21:49 -0300

    fix(path): fix path building to default language root page

--- src/util/pathBuilder.ts
+++ src/util/pathBuilder.ts
@@ -1,3 +1,3 @@
 export default function pathBuilder(...paths: string[]): string {
-  return paths.reduce((memo, path) => (path ? `${memo}/${path}` : memo), '');
+  return paths.reduce((memo, path) => (path ? `${memo}/${path}` : memo), '') || '/';
 }
